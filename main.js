3e92e6f119d2427a575b1a30f6243593cc26ddef
Mustafa-Niazi <mmnnhhee2001@gmail.com>
2026-01-06 18:47:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function shodata() {
     }
 
     document.getElementById('tbody').innerHTML = tablee;
-    document.getElementById('totalQuantity').innerText = dataprob.length;
+    document.getElementById('totalQuantity').innerhtml = dataprob.length;
     document.getElementById('totalPrice').innerText = total;
     document.getElementById('typeinfo1').innerText = `داخل: ${inCount}`;
     document.getElementById('typeinfo2').innerText = `خارج: ${outCount}`;
